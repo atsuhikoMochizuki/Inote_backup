83aa3969cea5ba5d8995b23434d6c0cf66818430
Atsuhiko Mochizuki <atsuhiko_mochizuki27@laposte.net>
2024-05-26 22:02:42 +0200

    Update runRestAPI_localDev.sh according to externalization of smtp4dev

--- runRestAPI_localDev.sh
+++ runRestAPI_localDev.sh
@@ -22,11 +22,11 @@ if [ -e "$apiPackage" ]; then
     echo -e "\033[32mOK\033[0m"
     
     echo -e -n "\033[93m -- Search Smtp4dev (smtp server mocking)...\033[0m"
-    if [ -f ./smtp-dev/Rnwood.Smtp4dev ]; then
+    if [ -f ../smtp-dev/Rnwood.Smtp4dev ]; then
         echo -e "\033[32mOK\033[0m"
         
         echo -e "\033[93m -- Launch Smtp-dev in new terminal : please enter root password\033[0m"
-        sudo gnome-terminal --title=SMTP4DEV --command "./smtp-dev/Rnwood.Smtp4dev"
+        sudo gnome-terminal --title=SMTP4DEV --command "../smtp-dev/Rnwood.Smtp4dev"
         xdotool windowminimize $(xdotool search --name SMTP4DEV|head -1)
         echo -e -n "\033[93m -- \033[0m"
         echo -e "\033[32mOK\033[0m"
@@ -36,7 +36,7 @@ if [ -e "$apiPackage" ]; then
         java -jar ./target/InoteApi-0.0.1-SNAPSHOT.jar
         cd ..
     else
-        echo -e "\033[31mNot Found : Smtp4dev should be present in current project\033[0m"
+        echo -e "\033[31mNot Found : This script expects to find a ‘smtp-dev’ folder in the folder containing the super project folder, which contains the Smtp4dev simulation smtp server. Please download Smtp4Dev and install it in the same folder as the Inote root superproject.\033[0m"
     fi
 else
     echo -e "\033[31mNot Found : have you build the package ?\033[0m"
