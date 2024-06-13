edef06ff44b5133cd16206fbd075fd4a673505ff
Atsuhiko Mochizuki <atsuhiko_mochizuki27@laposte.net>
2024-06-13 11:13:43 +0200

    2024-06-13 : 11:13:29 -- Global updating...

--- generalProjectUpdate.sh
+++ generalProjectUpdate.sh
@@ -16,6 +16,8 @@ echo "
   2024
  ==============================================================================                                 
 "
+now=$(date +"%Y-%m-%d : %H:%M:%S")
+
 echo -e -n "\033[93m -- Get last changes in submodules...\033[0m"
 git submodule update --remote --merge
 echo -e "\033[32mOK\033[0m"
@@ -25,7 +27,8 @@ git add .
 echo -e "\033[32mOK\033[0m"
 
 echo -e "\033[93m -- Commit last changes...\033[0m"
-git commit -m"$(date +'%H:%M:%S') -- Global updating..."
+NOW=$(date +"%H:%M:%S")
+git commit -m"$now -- Global updating..."
 echo -e "\033[32m OK\033[0m"
 
 echo -e "\033[93m -- Push last changes on default remote repository...\033[0m"
