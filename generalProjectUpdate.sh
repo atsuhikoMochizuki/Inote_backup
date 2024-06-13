0aa5e85afeb431eb2c12bf8286bc317b0ab783d2
Atsuhiko Mochizuki <atsuhiko_mochizuki27@laposte.net>
2024-06-13 11:05:46 +0200

    11:05:46 -- Global updating...

--- generalProjectUpdate.sh
+++ generalProjectUpdate.sh
@@ -32,6 +32,6 @@ echo -e "\033[93m -- Push last changes on default remote repository...\033[0m"
 git push origin main
 echo -e "\033[32m OK\033[0m"
 
-# echo -e -n "\033[93m -- Push last changes on mochizuki repository\033[0m"
-# git push mochizuki-portfolio main
-# echo -e "\033[32mOK\033[0m"
+echo -e "\033[93m -- Push last changes on mochizuki repository...\033[0m"
+git push mochizuki-portfolio main
+echo -e "\033[32m OK\033[0m"
